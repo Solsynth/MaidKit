984185f82d5e330ca673aa53210bd7be74b46357
LittleSheep <hi@littlesheep.me>
2026-08-17 17:01:34 +0800

    :bug: Fix android build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        coreLibraryDesugaringEnabled = true
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
